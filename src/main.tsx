abed0fc7a431036ef39f7c5384ed93096132aabc
guilhermealbanaz <guibragatoalbnz@gmail.com>
2024-12-12 08:46:45 -0300

    vite config

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { AppRoutes } from './presentation/routes';
 import './index.css';
 
+// Script para lidar com rotas no GitHub Pages
+(function(l) {
+  if (l.search[1] === '/' ) {
+    var decoded = l.search.slice(1).split('&').map(function(s) { 
+      return s.replace(/~and~/g, '&')
+    }).join('?');
+    window.history.replaceState(null, '',
+      l.pathname.slice(0, -1) + decoded + l.hash
+    );
+  }
+}(window.location));
+
 // Inicialização do Firebase
 const initFirebase = async () => {
   try {
