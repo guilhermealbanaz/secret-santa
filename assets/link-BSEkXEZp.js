05e5b88161fe8a8c793b73654c958003c64fa9f0
guilhermealbanaz <88803661+guilhermealbanaz@users.noreply.github.com>
2024-12-17 03:20:05 +0000

    Deploying to gh-pages from @ guilhermealbanaz/secret-santa@10f2b8a7af79bb0c5d98d1b55b0509af89b3ff03 🚀

--- assets/link-BSEkXEZp.js
+++ assets/link-BSEkXEZp.js
@@ -1,4 +1,4 @@
-import{c as a}from"./createLucideIcon-BvcpWxvl.js";/**
+import{c as a}from"./createLucideIcon-v1pGnHji.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
